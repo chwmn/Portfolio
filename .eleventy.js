67a3a3fbc2d5f69b4e403c69cfb467da4a28eb28
chulminh <60227860+chulminh@users.noreply.github.com>
2021-05-19 21:30:20 -0500

    added sass and css

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,28 @@
-module.exports = {
-  dir: {
-    input: "pre",
-    output: "dist",
-  },
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget("./src/css/");
+  eleventyConfig.setTemplateFormats([
+    "css",
+    "html",
+    "liquid",
+    "jpg",
+    "png",
+    "svg",
+    "js",
+  ]);
+
+  function sortByPageOrder(values) {
+    return values.slice().sort((a, b) => a.data.order - b.data.order);
+  }
+
+  eleventyConfig.addFilter("sortByPageOrder", sortByPageOrder);
+
+  // You can return your Config object (optional).
+  return {
+    dir: {
+      includes: "_includes",
+      layouts: "_layouts",
+      input: "src",
+      output: "dist",
+    },
+  };
 };
