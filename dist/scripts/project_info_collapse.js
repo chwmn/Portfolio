c2ace10737ff9c987589bf4990a427febff32a22
chwmn <chulminh99@gmail.com>
2021-08-24 15:33:29 -0500

    got project info button to work

---
+++ dist/scripts/project_info_collapse.js
@@ -0,0 +1,30 @@
+let button = document.querySelectorAll(".plus");
+
+for (let i = 0; i < button.length; i++) {
+  let display = new Boolean(false);
+
+  button[i].onclick = () => {
+    info = button[i].parentElement.previousElementSibling.querySelectorAll("p");
+    if (display == false) {
+      button[i].classList.remove("open");
+      button[i].classList.add("close");
+
+      for (let i = 0; i < info.length; i++) {
+        info[i].classList.remove("hidden");
+        info[i].classList.add("shown");
+        console.log(info[i]);
+      }
+
+      display = true;
+    } else if (display == true) {
+      button[i].classList.remove("close");
+      button[i].classList.add("open");
+
+      for (let i = 0; i < info.length; i++) {
+        info[i].classList.add("hidden");
+        info[i].classList.remove("shown");
+      }
+      display = false;
+    }
+  };
+}
