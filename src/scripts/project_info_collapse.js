be16e0aeedbd65123074c6723df88e145e565037
chwmn <chulminh99@gmail.com>
2021-08-25 17:48:46 -0500

    cleaning up the collapse thingy and overall eresponsive

--- src/scripts/project_info_collapse.js
+++ src/scripts/project_info_collapse.js
@@ -4,7 +4,12 @@ for (let i = 0; i < button.length; i++) {
   let display = new Boolean(false);
 
   button[i].onclick = () => {
-    info = button[i].parentElement.previousElementSibling.querySelectorAll("p");
+    info =
+      button[i].parentElement.previousElementSibling.querySelectorAll(".info");
+    // headInfo =
+    //   button[i].parentElement.previousElementSibling.querySelectorAll("h3");
+    // processInfo = button[i].previousElementSibling;
+
     if (display == false) {
       button[i].classList.remove("open");
       button[i].classList.add("close");
@@ -12,9 +17,15 @@ for (let i = 0; i < button.length; i++) {
       for (let i = 0; i < info.length; i++) {
         info[i].classList.remove("hidden");
         info[i].classList.add("shown");
-        console.log(info[i]);
       }
 
+      // for (let i = 0; i < headInfo.length; i++) {
+      //   headInfo[i].classList.remove("hide");
+      //   headInfo[i].classList.add("show");
+      // }
+
+      // processInfo.classList.remove("hidden");
+      // processInfo.classList.add("shown");
       display = true;
     } else if (display == true) {
       button[i].classList.remove("close");
@@ -24,6 +35,14 @@ for (let i = 0; i < button.length; i++) {
         info[i].classList.add("hidden");
         info[i].classList.remove("shown");
       }
+
+      // for (let i = 0; i < headInfo.length; i++) {
+      //   headInfo[i].classList.remove("show");
+      //   headInfo[i].classList.add("hide");
+      // }
+
+      // processInfo.classList.remove("shown");
+      // processInfo.classList.add("hidden");
       display = false;
     }
   };
